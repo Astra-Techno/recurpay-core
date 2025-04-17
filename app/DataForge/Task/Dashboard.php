afc0f48dcc77c81afd2f6c97544376c3d1f08333
vgkumar12-sys <58596030+vgkumar12-sys@users.noreply.github.com>
2025-04-18 00:05:34 +0530

    Dashboard Stats Backend Coding

--- app/DataForge/Task/Dashboard.php
+++ app/DataForge/Task/Dashboard.php
@@ -10,17 +10,25 @@ class Dashboard extends Task
     public function statsList($request)
     {
 
-       $data = [];
+        $data = [];
 
-        $data['stats'] =  [
+        $data['stats'] = [
             'Total Properties' => \Sql('Properties', ['select_type' => 'total'])->result() ?? 0,
-            'Occupied Units'=> \Sql('Properties:activeProperties', ['select_type' => 'total','status' => 'active'])->result() ?? 0,
-            'Pending Payments' => \Sql('PaymentTransactions', ['select_type' => 'total','status' => 'pending'])->result() ?? 0,
-            'Revenue'=> \Sql('PaymentTransactions', ['select_type' => 'revenue'])->result() ?? 0.00
+            'Occupied Units' => \Sql('Properties:activeProperties', ['select_type' => 'total', 'status' => 'active'])->result() ?? 0,
+            'Pending Payments' => \Sql('PaymentTransactions', ['select_type' => 'total', 'status' => 'pending'])->result() ?? 0,
+            'Revenue' => \Sql('PaymentTransactions', ['select_type' => 'revenue'])->result() ?? 0.00
         ];
 
-        $data['occupancy'] = [\Sql('Properties:activeProperties', ['select_type' => 'total','status' => 'active'])->result() ?? 0,
-            \Sql('Properties:activeProperties', ['select_type' => 'total','status' => 'vacant'])->result() ?? 0];
+        $data['occupancy'] = [
+            \Sql('Properties:activeProperties', ['select_type' => 'total', 'status' => 'active'])->result() ?? 0,
+            \Sql('Properties:activeProperties', ['select_type' => 'total', 'status' => 'vacant'])->result() ?? 0
+        ];
+
+        $data['statsCount'] = [
+            'totalProperties' => \Sql('Properties', ['select_type' => 'total'])->result() ?? 0,
+            'tenants' => \Sql('Tenants', ['select_type' => 'total'])->result() ?? 0,
+            'pendingPayments' => \Sql('PaymentTransactions', ['select_type' => 'total', 'status' => 'pending'])->result() ?? 0
+        ];
 
 
         return $data;
