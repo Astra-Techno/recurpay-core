94e89b51627d9fa4088300e435d2c4488c235d01
vgkumar12-sys <58596030+vgkumar12-sys@users.noreply.github.com>
2025-04-26 09:00:36 +0530

    Backend Changes for Properties Listing

--- app/DataForge/Sql/Payments.php
+++ app/DataForge/Sql/Payments.php
@@ -10,7 +10,7 @@ class Payments extends Sql
     public function default(&$data)
     {
         $query = Query('PaymentList');
-        $query->select('list', "pp.id, p.name AS property, pp.amount, pp.currency, pp.period, 
+        $query->select('list', "pp.id,p.id as property_id, p.name AS property, pp.amount, pp.currency, pp.period,
                         pp.total_due, pp.next_due_date, p.address1, DATEDIFF(pp.next_due_date, CURDATE()) AS due_in_days,
                         CASE WHEN pp.type='other' AND pp.other_type !='' THEN pp.other_type
                         ELSE pp.type END AS type");
@@ -32,6 +32,32 @@ class Payments extends Sql
             '{request.Due}=1 AND pp.total_due > 0 AND pt.user_id='.Auth::id(),
             '{request.Pending}=1 AND pp.total_due > 0 AND p.user_id='.Auth::id()
         ]);
+
+        $query->group('pp.id');
+
+        $query->order('pp.next_due_date', 'ASC');
+
+        return $query;
+    }
+
+    public function allPayments($data)
+    {
+        $query = Query('PaymentList');
+        $query->select('list', "pp.id,p.id as property_id, p.name AS property, pp.amount, pp.currency, pp.period,
+                        pp.total_due, pp.next_due_date, p.address1, DATEDIFF(pp.next_due_date, CURDATE()) AS due_in_days,
+                        CASE WHEN pp.type='other' AND pp.other_type !='' THEN pp.other_type
+                        ELSE pp.type END AS type");
+        $query->select('entity', 'pp.*, p.name AS property, GROUP_CONCAT(pu.user_id) AS userIds');
+        $query->select('total', 'COUNT(pp.id) AS total');
+
+        $query->from('payment_users AS pu');
+        $query->inner('payments AS pp ON pu.payment_id = pp.id');
+        $query->inner('properties AS p ON p.id = pp.property_id');
+        $query->inner('property_tenants AS pt ON pt.user_id=pu.user_id');
+
+        $query->filter('pu.status = 1');
+        $query->filter('p.user_id = '.Auth::id().' OR pu.user_id = '.Auth::id());
+
 
         $query->group('pp.id');
 
